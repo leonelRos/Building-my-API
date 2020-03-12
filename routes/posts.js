20113687ac93f6c4d0d6102d846c66bf262cfc5e
Leonel Rosales <rleonel426@gmail.com>
2020-03-11 17:33:18 -0700

    getting a specific post

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,17 @@
 var express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
-// request all the post
-router.get('/', (req, res) => {
-  // res.send('we are in posts')
-  const posts = new Post({
 
-  })
+// gets all the post using promises
+router.get('/', async (req, res) => {
+try {
+  const posts = await Post.find();
+  res.json(posts);
+} catch (err){
+  res.json({message: err})
+  }
 });
-// this submit a post
+// this submit a post with promises
 router.post('/',  async (req,res) => {
   const post =  new Post({
     title: req.body.title,
@@ -27,10 +30,14 @@ router.post('/',  async (req,res) => {
 // route for a specific posts using promises!
 router.get('/:postId', async (req, res) => {
   try {
+    // we use the "Post", we require after requiring express
     const post = await Post.findById(req.params.postId);
     res.json(post);
   } catch (err){
     res.json({ message:err })
   }
 })
+// deleteing a posts usign promises
+router.delete('', )
+
 module.exports = router;
